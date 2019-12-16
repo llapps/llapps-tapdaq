df7af381e5db648a5125678b7548c6678178e879
llapps <lauhoffo@gmail.com>
2019-12-16 11:59:52 +0100

    Create tapdaq.java

---
+++ src/tapdaq.java
@@ -0,0 +1,22 @@
+package com.creativeads.tapdaq;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+public class Adder extends CordovaPlugin {
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("performAdd")) {
+            int arg1 = args.getInt(0);
+            int arg2 = args.getInt(1);
+            /* Indicating success is failure is done by calling the appropriate method on the 
+            callbackContext.*/
+            int result = arg1 + arg2;
+            callbackContext.success("result calculated in Java: " + result);
+            return true;
+        }
+        return false;
+    }
+}
